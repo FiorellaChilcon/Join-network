15139ffc36a7a283c4e29446163623e1d283e164
fiorellachilcon <fiorellachilc_20@hotmail.com>
2021-12-18 00:25:23 -0500

    delete comment feature

--- src/components/Home/Comment.js
+++ src/components/Home/Comment.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState, useMemo } from 'react'
 import userAvatar from '../../assets/images/user-avatar.svg';
 import { useAuth } from '../../contexts/AuthContext';
 import ReactTimeAgo from 'react-time-ago';
+import menu from '../../assets/images/menu-dots.svg';
 
 export default function Comment(props) {
   const { comment } = props;
   const data = comment.data();
   const [userComment, setUserComment] = useState({});
-  const { currentUser, getUserDoc } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const { currentUser, getUserDoc, removeDoc, addToastMessage } = useAuth();
+  const [showMenu, setShowMenu] = useState(false);
+  const myComment = data.userId === currentUser.uid;
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -32,6 +36,24 @@ export default function Comment(props) {
     return displayName ? displayName : email?.split('@')[0];
   }, [userComment]);
 
+  const handleShowMenu = () => {
+    if (!isLoading) {
+      setShowMenu((prev) => !prev);
+    }
+  }
+
+  const handleDeletePost = async () => {
+    try {
+      setIsLoading(true);
+      setShowMenu(false);
+      await removeDoc('comments', comment.id);
+      addToastMessage('Your comment was deleted successfully', 'success');
+    } catch(error) {
+      setIsLoading(false);
+      addToastMessage(error.message, 'error');
+    }
+  }
+
 
   return (
     <div className='comment'>
@@ -39,9 +61,27 @@ export default function Comment(props) {
         <img className='fit-img' src={userComment.photoURL || userAvatar} alt='user'/>
       </div>
       <div className='content'>
-        <div className='name'>
-          {userName}
-          <span> · <ReactTimeAgo date={data.createdAt} timeStyle='twitter' locale="en-US"/></span>
+        <div className='flex'>
+          <div className='name'>
+            {userName}
+            <span> · <ReactTimeAgo date={data.createdAt} timeStyle='twitter' locale="en-US"/></span>
+          </div>
+
+          {myComment &&
+            <div className='menu-container pointer'>
+              <img
+                onClick={handleShowMenu}
+                className={`dot-menu ${isLoading && 'not-allowed'}`}
+                src={menu}
+                alt='post menu'
+              />
+              {showMenu &&
+                <div className='menu'>
+                  {/* <NavLink to={`/post/${comment.id}/edit`}>Edit</NavLink> */}
+                  <div role='button' onClick={handleDeletePost}>Delete</div>
+                </div>}
+            </div>
+          }
         </div>
         <div className='text'>
           {data.content}
